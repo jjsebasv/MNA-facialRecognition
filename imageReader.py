7c57fae569d395e9aaae6cd2cfe305e0941017ce
heyimnowi <noelia.lopez@mercadolibre.com>
2017-09-02 12:15:45 -0300

    Convert image to matrix

--- imageReader.py
+++ imageReader.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def main():
     imageMatrix = np.asarray(Image.open('images/s1/1.pgm').convert('L'))
-    print(imageMatrix)
+    print(imageMatrix )
 
 if __name__ == "__main__":
     main()
