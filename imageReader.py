c41f84a648c4f36f4956add84456a931398a4bb6
heyimnowi <noelia.lopez@mercadolibre.com>
2017-09-02 12:14:05 -0300

    Adding faces

---
+++ imageReader.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from PIL import Image
+import numpy as np
+
+def main():
+    imageMatrix = np.asarray(Image.open('images/s1/1.pgm').convert('L'))
+    print(imageMatrix)
+
+if __name__ == "__main__":
+    main()
