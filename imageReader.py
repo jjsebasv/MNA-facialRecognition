0377fe839964b883483d7416d70b5480208a922b
heyimnowi <noelia.lopez@mercadolibre.com>
2017-09-02 12:53:06 -0300

    Get training faces

--- imageReader.py
+++ imageReader.py
@@ -2,10 +2,20 @@
 
 from PIL import Image
 import numpy as np
+from pathlib import Path
+
+def getTrainingFaces():
+    trainingFaces = []
+    imagesList = Path("images").glob('**/*.pgm')
+
+    for image in imagesList:
+        imageMatrix = np.asarray(Image.open(str(image)).convert('L'))
+        imageMatrixReshaped = imageMatrix.reshape(-1, 1);
+        trainingFaces.append(imageMatrixReshaped);
+        print(imageMatrixReshaped.shape)
 
 def main():
-    imageMatrix = np.asarray(Image.open('images/s1/1.pgm').convert('L'))
-    print(imageMatrix )
+    getTrainingFaces()
 
 if __name__ == "__main__":
     main()
