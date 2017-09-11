86328678861865f2726fefc1832a7381b69e7697
Sebastian Vera <jj.sebas.v@gmail.com>
2017-09-11 15:51:53 -0300

    No estoy seguro si esto esta bien

--- main.py
+++ main.py
@@ -8,11 +8,19 @@ def main():
     # cada face_vector es de (1, 112*92) = (1, 10304)
     face_vectors = calculate_face_vectors(training_faces)
     average_face_vect = average_face_vector(face_vectors)
-    face_vectors_minus_avg = face_vectors - average_face_vect
-    covariance_matrix = np.cov(face_vectors_minus_avg.transpose())
+    face_vectors_minus_avg = np.matrix(face_vectors - average_face_vect)
 
+    # filas < columnas
+    if (face_vectors_minus_avg.shape[0] < face_vectors_minus_avg.H.shape[0]):
+        S = np.matrix(face_vectors_minus_avg) * np.matrix(face_vectors_minus_avg).H
+    # columnas < filas
+    else:
+        S = np.matrix(face_vectors_minus_avg).H * np.matrix(face_vectors_minus_avg)
 
-    # eig = calculate_eigenvalues(covariance_matrix)
+    covariance_matrix = np.matrix(np.cov(S))
+    eig = calculate_eigenvalues(covariance_matrix)
+
+    print(eig)
     # pca(as_row_matrix(A))
 
 
